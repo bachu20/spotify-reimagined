ee803589a74d2529171d6319c8aca88847a62b41
Brandon Achu <brandon.achu@expel.io>
2024-01-30 19:58:50 -0500

    Feature: Implement Search

---
+++ app/search/components/search-content.tsx
@@ -0,0 +1,32 @@
+"use client";
+
+import MediaItem from "@/components/media-item";
+import { Song } from "@/types/common";
+
+interface Props {
+  songs: Song[];
+}
+
+const SearchContent: React.FC<Props> = ({ songs }) => {
+  if (!songs.length) {
+    return (
+      <div className="flex flex-col gap-y-2 w-full px-6 text-neutral-400">
+        No songs found.
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col gap-y-2 w-full px-6">
+      {songs.map((song) => (
+        <div key={song.id} className="flex items-center gap-x-4 w-full">
+          <MediaItem onClick={() => {}} song={song} />
+        </div>
+      ))}
+
+      {/* TODO: Add like button later */}
+    </div>
+  );
+};
+
+export default SearchContent;
